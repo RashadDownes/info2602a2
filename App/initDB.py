29be77769d1967000c9935e9d3cc010077d35f79
RashadDownes <rashad.downes@my.uwi.edu>
2021-03-24 13:49:15 +0000

    new change

--- App/initDB.py
+++ App/initDB.py
@@ -11,15 +11,15 @@ with open('../data.json') as json_file:
         data = Datas(
             name = pokemon['name'],  
             id = pokemon['id'], 
-            attack = pokemon['stats']['attack']
-            defense =  pokemon['stats']['defense']
-            hp = pokemon['stats']['hp']
-            height = pokemon['physique']['height']
-            sp_attack = pokemon['specials']['sp_attack']
-            sp_defense = pokemon['specials']['sp_defense']
-            speed = pokemon['stats']['speed']
-            type1 = pokemon['type']['type1']
-            type2 = pokemon['type']['type2']
+            attack = pokemon['stats']['attack'],
+            defense =  pokemon['stats']['defense'],
+            hp = pokemon['stats']['hp'],
+            height = pokemon['physique']['height'],
+            sp_attack = pokemon['specials']['sp_attack'],
+            sp_defense = pokemon['specials']['sp_defense'],
+            speed = pokemon['stats']['speed'],
+            type1 = pokemon['type']['type1'],
+            type2 = pokemon['type']['type2'],
             weight = pokemon['physique']['weight']
         )
         db.session.add(data)
