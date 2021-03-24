e12aa42467be99e86c24c89c4131d01a1c01b867
RashadDownes <rashad.downes@my.uwi.edu>
2021-03-24 17:35:17 +0000

    new change

--- App/initDB.py
+++ App/initDB.py
@@ -12,9 +12,9 @@ db.create_all(app=app)
 #         print(name)
 
 with open('pokemon.csv', mode='r') as csv_file:
-    Pokemons = csv.DictReader(csv_file)
-    for pokemon in Pokemons:
-        data = Pokemon(
+    pokemonFile = csv.DictReader(csv_file)
+    for pokemon in pokemonFile:
+        pokemons = Pokemon(
             pid = pokemon["pokedex_number"], 
             name = pokemon["name"],
             attack = pokemon["attack"],
@@ -26,10 +26,10 @@ with open('pokemon.csv', mode='r') as csv_file:
             speed = pokemon["speed"],
             type1 = pokemon["type1"],
             type2 = pokemon["type2"],
-            weight = pokemon['weight_kg']
+            weight = pokemon["weight_kg"]
         )
-        print(data)
-        db.session.add(data)
+        print(pokemons)
+        db.session.add(pokemons)
     db.session.commit()
 
 # replace any null values with None to avoid db errors
